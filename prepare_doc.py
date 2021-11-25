916c3188f009f424632cb7a2319cc9fa9a61b6af
MustafaAbuGhannam <mustafa99au@gmail.com>
2021-11-25 15:22:32 +0200

    deleted some usless code for the file

--- prepare_doc.py
+++ prepare_doc.py
@@ -71,38 +71,3 @@ def prepare_doc_test(path):
                 'data1_as_one_page/{}.jpeg'.format(file.split('.')[0]))
 
     return path_1, path_2
-
-
-# def prepare_document():
-#     dirName = 'files_after_concat'  # 'connected files'
-#     files = os.listdir('final_test/')
-#     file_number = 1
-#     i = 0
-#     for file in files:
-#         i += 1
-#         img = Image.open('final_test/' + str(file))
-#         # file = os.path.splitext(file)[0] + ".jpeg"
-#         array_images = []
-#         try:
-#             page1 = cut_width(img, 1)
-#             array_images.append(page1)
-#             img.seek(1)
-#             page2 = cut_width(img, 2)
-#             array_images.append(page2)
-#         except EOFError:
-#             break
-#
-#         # Create target Directory if don't exist
-#         if not os.path.exists(dirName):
-#             os.mkdir(dirName)
-#         if file_number == 1:
-#             concat_1 = get_concat_vertical(page1, page2)
-#             concat_1.save('final_test_files/page_' + str(i) + '.jpeg')
-#             file_number = 2
-#         else:
-#             concat_2 = get_concat_vertical(page1, page2)
-#             concat_2.save('final_test_files/page_' + str(i) + '.jpeg')
-
-
-# if __name__ == "__main__":
-#     prepare_document()
