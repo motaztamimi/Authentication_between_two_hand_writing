3e59ae165bfa56c94bc6fcc7ade73efeb1c734f8
MustafaAbuGhannam <80034478+MustafaAbuGhannam@users.noreply.github.com>
2022-04-21 18:24:40 +0300

    impleminting triplt loss

--- triplet_model.py
+++ triplet_model.py
@@ -1,20 +1,25 @@
 import torch
 import torch.nn as nn
-from data_set import LinesDataSetTriplet
+from data_set import LinesDataSetTriplet, LinesDataSet
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
 from matplotlib import pyplot as plt
+import numpy as np
 class TriplteLoss(nn.Module):
     "Triplet loss function"
-    def __init__(self, margin=2):
+    def __init__(self, margin=1):
         super(TriplteLoss, self).__init__()
         self.margin = margin
             
     def forward(self, anc, pos, neg):
-        d_a_p = torch.pairwise_distance(anc, pos)
-        d_a_n = torch.pairwise_distance(anc, neg)
-
-        loss = torch.sum(torch.max(d_a_p - d_a_n + self.margin, torch.tensor(0.)))
+        d_a_p = anc - pos
+        d_a_n = anc - neg
+        d_a_p_sq = torch.sum(torch.pow(d_a_p, 2), 1)
+        d_a_n_sq = torch.sum(torch.pow(d_a_n, 2), 1)
+        d_a_p = torch.sqrt(d_a_p_sq)
+        d_a_n = torch.sqrt(d_a_n_sq)
+        m_dist = torch.clamp(d_a_p - d_a_n + self.margin, min=0.0)
+        loss = torch.sum(m_dist) / anc.size()[0]
         return loss
         
 
@@ -95,11 +100,14 @@ class ResNet(nn.Module):
         # out = self.fc1(out)
         return out
 
-    def forward(self, input1, input2, input3):
+    def forward(self, input1, input2, input3=None):
         output1 = self.forward_once(input1)
         output2 = self.forward_once(input2)
-        output3 = self.forward_once(input3)
-        output = torch.stack((output1, output2, output3))
+        if input3 != None:
+            output3 = self.forward_once(input3)
+            output = torch.stack((output1, output2, output3))
+        else:
+            output = torch.stack((output1, output2))
         return output
 
 
@@ -118,25 +126,62 @@ def triplet_train( model, loss_function, optimizer,train_loader,loss_history, ep
 
         output = model(image1, image2, image3)
         optimizer.zero_grad()
-        print(output.size())
         loss = loss_function(output[0], output[1], output[2])
         loss_history.append(loss.cpu().item())
         batches_loss.append(loss.cpu().item())
         loss.backward()
         optimizer.step()
-    # loss_history_for_ephoces.append( sum(batches_loss) / len(batches_loss))
-        print(sum(batches_loss) / len(batches_loss))
+        loss_history_for_epoch.append( sum(batches_loss) / len(batches_loss))
+
+
+def test( model,test_loader):
+    model.eval()
+    for _ , data in enumerate(test_loader):
+        image1, image2, label = data
+        image1 = image1.float().cuda()
+        image2 = image2.float().cuda()
+        image1 = image1[:,None,:,:]
+        image2 = image2[:,None,:,:]
+        label = label.cuda()
+        with torch.no_grad():
+            output = model(image1, image2)
+            dist_ = output[0][0] - output[1][0]
+            dist_ = torch.sum(torch.pow(dist_, 2))
+            dist_ = torch.sqrt(dist_)
+            dist_with_label.append((dist_.cpu(), label.cpu()))
 
 
 if __name__ == '__main__':
     train_line_data_set = LinesDataSetTriplet(csv_file="train_triplet.csv", root_dir="data_for_each_person", transform=transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,),(0.5,))]))
-    test_line_data_set = LinesDataSetTriplet(csv_file="test_triplet.csv", root_dir='data_for_each_person',  transform=transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,),(0.5,))]))
-    train_line_data_loader = DataLoader(train_line_data_set,shuffle=True,batch_size=20)
+    test_line_data_set = LinesDataSet(csv_file="Test_labels_for_arabic.csv", root_dir='data_for_each_person',  transform=transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,),(0.5,))]))
+    train_line_data_loader = DataLoader(train_line_data_set, shuffle=True, batch_size=20)
+    test_line_data_loader = DataLoader(test_line_data_set, shuffle=True, batch_size=1)
     my_model = ResNet(ResidualBlock, [2, 2, 2]).cuda()
     loss_function = TriplteLoss()
     optimizer = torch.optim.Adam(my_model.parameters(), lr=0.001)
-    for i in range(2):
+    
+    
+    for i in range(20):
+        dist_with_label = []
+        loss_history_for_epoch = []
+        print(f"train epoch: {i}")
         triplet_train(my_model, loss_function, optimizer, train_line_data_loader, loss_history=[], epoch=0)
+        print(f'epoch loss: {sum(loss_history_for_epoch) / len(loss_history_for_epoch)}')
+        torch.save(my_model.state_dict(), 'model_triplt.pt')
+        my_model.load_state_dict(torch.load('model_triplt.pt', map_location='cuda:0'))
+        print(f"test epoch: {i}")
+        test(my_model, test_line_data_loader)
+        dict_ = {i: 0 for i in list(np.arange(0.5, 20.5, 0.5))}
+        for i in list(np.arange(0.5, 20.5, 0.5)):
+            for dist, label in dist_with_label:
+                if dist.item() <= i and label.item() == 0.:
+                    dict_[i] += 1
+                elif dist.item() > i and label.item() == 1.:
+                    dict_[i] += 1 
+        print(f"test results: {dict_}")            
+
+
+    
 
         
         
