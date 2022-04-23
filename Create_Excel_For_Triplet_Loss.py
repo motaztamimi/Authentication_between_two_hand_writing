bc9ae9f25d8740a2df5c03e11ce3f3b3ba2d5a18
motaztamimi <74739600+motaztamimi@users.noreply.github.com>
2022-04-23 21:11:58 +0300

    finishing triplet loss

--- Create_Excel_For_Triplet_Loss.py
+++ Create_Excel_For_Triplet_Loss.py
@@ -42,6 +42,6 @@ if __name__ == "__main__":
 
     train_file  = pd.read_excel('1-230Arabic.xlsx')
     test_file = pd.read_excel('50D_test_arabic.xlsx')
-    create_csv(train_file, 10000, 'train_triplet.csv')
-    create_csv(test_file, 4000, 'test_triplet.csv')
+    create_csv(train_file, 25000, 'train_triplet.csv')
+    create_csv(test_file, 8000, 'test_triplet.csv')
     
--- playgorund.py
+++ playgorund.py
@@ -7,11 +7,5 @@ from time import sleep
 from traceback import print_tb
 
 
-list_of_lists = [[1] for i in range(10)]
-list_of_lists1 = [[1]] * 10 
-
-list_of_lists[0][0] = 10
-list_of_lists1[0][0] = 10
-
+list_of_lists = { i: i+1 for i in range(10)}
 print(list_of_lists)
-print(list_of_lists1)
